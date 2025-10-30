f026afb4c8b3c3f8315a265f39d7e13a636eb27e
emamjion <emamjion@gmail.com>
2025-10-30 20:20:38 +0600

    About section design completed

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,12 +1,14 @@
 "use client";
 import { motion } from "framer-motion";
 import Image from "next/image";
+import AnimatedText from "./AnimatedText";
 
 const About = () => {
   return (
     <section className="relative pt-12 pb-24" id="about">
       <div className="container mx-auto h-full">
         <div className="h-full flex items-center justify-center">
+          {/* image + shapes */}
           <div className="hidden xl:flex flex-1 pl-8">
             <div className="relative w-full max-w-[380px]">
               {/* shape */}
@@ -49,6 +51,45 @@ const About = () => {
                     Years of <br /> Experience
                   </div>
                 </div>
+              </div>
+            </div>
+          </div>
+          {/* text */}
+          <div className="text-center xl:text-left w-full xl:w-[50%] mx-auto xl:mx-0 flex flex-col gap-6">
+            <div>
+              <AnimatedText
+                text="My Name is Emam Khalid Jion"
+                textStyles="mb-2 text-primary text-[36px] tracking-[-0.02em] leading-[44px] font-semibold"
+              />
+              <p className="text-lg">Web & App Developer</p>
+            </div>
+            <p className="max-w-[680px] mx-auto xl:mx-0 mb-2">
+              I create visually stunning and functionai websites and app using
+              modern technologies and design principles. Explore my work to see
+              how I combine creativity with technical skill to deliver
+              exceptional digital experiences.
+            </p>
+            {/* info items */}
+            <div className="flex flex-col lg:flex-row gap-8 xl:gap-12 max-w-max mx-auto xl:mx-0 items-center">
+              {/* item 1 */}
+              <div className="max-w-max">
+                <div className="uppercase font-bold text-primary">Age</div>
+                <p>27 Years</p>
+              </div>
+              {/* item 2 */}
+              <div className="max-w-max">
+                <div className="uppercase font-bold text-primary">Born in</div>
+                <p>Bangladesh</p>
+              </div>
+              {/* item 3 */}
+              <div className="max-w-max">
+                <div className="uppercase font-bold text-primary">Phone</div>
+                <p>01727087717</p>
+              </div>
+              {/* item 4 */}
+              <div className="max-w-max">
+                <div className="uppercase font-bold text-primary">Email</div>
+                <p>emamjion@gmail.com</p>
               </div>
             </div>
           </div>
